116de7f610cf87eadc13859191549d445ab1ab8f
Abhiraj Sinha <emailforabhiraj369@gmail.com>
2026-06-05 22:32:21 +0530

    feat: implement useEffect and localStorage for persistent state

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
@@ -6,23 +6,33 @@ import './App.css'
 
 
 
-const INITIAL_JOBS = [
-  { id: 1, company: "Google", role: "Frontend Engineer", status: "Applied" },
-  { id: 2, company: "Meta", role: "React Developer", status: "Interviewing" },
-  { id: 3, company: "Netflix", role: "UI Engineer", status: "Rejected" }
-];
 
-const newJob = { id: 4, company: "Apple", role: "iOS Developer", status: "Applied" };
+
+
 function App() {
 
-  const [jobs, setJobs] = useState(INITIAL_JOBS);
+
 
   const [companyInput, setCompanyInput] = useState("");
   const [roleInput, setRoleInput] = useState("");
-  const [statusInput,setStatusInput]=useState("Applied");
+  const [statusInput, setStatusInput] = useState("Applied");
+  const [applications, setApplications] = useState(() => {
+
+    const savedApps = localStorage.getItem("dev-tracker-apps");
+    console.log('1. Reading from localStorage on load:', savedApps)
+    return savedApps ? JSON.parse(savedApps) : [];
+  });
+  // Day5 using local storage and useEffect
+  useEffect(() => {
+    console.log("2. useEffect running.Saving to localStorage:", applications);
+    localStorage.setItem("dev-tracker-apps", JSON.stringify(applications))
+  }, [applications]);
+
 
   function addJob() {
 
+    //check to not add empt job or jo roles
+    if (!companyInput || !roleInput) return;
     const customJob = {
       id: Date.now(),
       company: companyInput,
@@ -30,7 +40,7 @@ function App() {
       status: statusInput
 
     }
-    setJobs([...jobs, customJob]);
+    setApplications([...applications, customJob]);
 
     setCompanyInput("");
 
@@ -38,41 +48,51 @@ function App() {
     setStatusInput("Applied");
   }
 
+
+
+
   return (
     <>
-      <h1>Day4 :Job Tracker</h1>
+      <h1>Day 5 :Job Tracker</h1>
+      {/* Displaying the application list */}
       <ul>
-        {jobs.map((joblist, index) => {
-          const { id, company, role, status } = joblist;
-          return <li key={id}>{company}  {role} - {status}</li>
-        }
-        )}
+        {applications.map((job) => {
+          const { id, role, company } = job;
+          return <li key={id}>  <strong>{company}</strong> - {role}
+          </li>
+        })}
       </ul>
+
       {/* custom company input */}
 
       <input type="text"
         placeholder='Enter Company name'
         value={companyInput}
         onChange={(e) => setCompanyInput(e.target.value)} />
-       <br />
+      <br />
 
-        {/* custom role input */}
+      {/* custom role input */}
 
-        <input type="text" 
+      <input type="text"
         placeholder='Enter job role'
         value={roleInput}
-        onChange={(e)=>setRoleInput(e.target.value)}/>
-        <br />
-        <select value={statusInput} 
-        onChange={(e)=>setStatusInput(e.target.value)}
-        >
-          <option value="Applied">Applied</option>
-          <option value="Interviewing">Interviewing</option>
-          <option value="Offered">Offered</option>
-          <option value="Rejected">Rejected</option>
-        </select>
+        onChange={(e) => setRoleInput(e.target.value)} />
+      <br />
+      <select value={statusInput}
+        onChange={(e) => setStatusInput(e.target.value)}
+      >
+        <option value="Applied">Applied</option>
+        <option value="Interviewing">Interviewing</option>
+        <option value="Offered">Offered</option>
+        <option value="Rejected">Rejected</option>
+      </select>
+      <div style={{ padding: '20px' }}>
+        <button onClick={addJob}>Add Job</button>
 
-      <button onClick={addJob}>Add Job</button>
+
+
+
+      </div>
     </>
   )
 }
